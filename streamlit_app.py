68b2b8f9f4be8d30bddab0c09e1331a09c456a84
ignaciojimenez-dev <ignacio.jimenez@sdggroup.com>
2025-12-23 16:21:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -15,7 +15,12 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 # Conexión a Snowflake
-session = get_active_session()
+# CAMBIAR ESTO:
+# session = get_active_session()
+
+# POR ESTO:
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 # El Selector de Frutas
